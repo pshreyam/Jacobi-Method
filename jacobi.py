a554ac3c099188b9408fcd237083803dea9fd6cd
pshreyam <sp02434318@student.ku.edu.np>
2020-11-20 21:58:24 +0545

    removed tabs/ added spaces

--- jacobi.py
+++ jacobi.py
@@ -4,41 +4,41 @@ from prettytable import PrettyTable
 table = PrettyTable()
 
 class JacobiMethod:
-	def __init__(self, coeff_matrix, const_vector, initial=None, tolerance=0.00005):
-		self.coeff_matrix = coeff_matrix
-		self.const_vector = const_vector
-		self.solution = initial or [0 for _ in self.const_vector] 
-		self.tolerance = tolerance
-		
-	def calculate(self):
-		n = len(self.solution)
-		x_new = [0 for i in self.solution]
+    def __init__(self, coeff_matrix, const_vector, initial=None, tolerance=0.00005):
+        self.coeff_matrix = coeff_matrix
+        self.const_vector = const_vector
+        self.solution = initial or [0 for _ in self.const_vector]
+        self.tolerance = tolerance
 
-		table.title = "Iteration Table"		
-		table.field_names = ['n', *[f'x{i}' for i in range(1, n+1)]]	
+    def calculate(self):
+        n = len(self.solution)
+        x_new = [0 for i in self.solution]
 
-		iteration = 1
-		while True:
-			table.add_row([iteration, *[f'{value:.5f}' for value in self.solution]])
-			
-			for i in range(n):
-				_sum = 0
-				for j in range(n):
-					if not i == j:
-						_sum -= self.coeff_matrix[i][j] * self.solution[j] 
-					else:
-						_sum += self.const_vector[i]		
-				x_new[i] = _sum / self.coeff_matrix[i][i]
-				
-			if max(abs(np.array(self.solution)-np.array(x_new))) < self.tolerance:
-				break
-				
-			self.solution = [round(x, 5) for x in x_new]
-			iteration += 1
+        table.title = "Iteration Table"
+        table.field_names = ['n', *[f'x{i}' for i in range(1, n+1)]]
 
-		print(table)		
+        iteration = 1
+        while True:
+            table.add_row([iteration, *[f'{value:.5f}' for value in self.solution]])
 
-		
+            for i in range(n):
+                _sum = 0
+                for j in range(n):
+                    if not i == j:
+                        _sum -= self.coeff_matrix[i][j] * self.solution[j]
+                    else:
+                        _sum += self.const_vector[i]
+                x_new[i] = _sum / self.coeff_matrix[i][i]
+
+            if max(abs(np.array(self.solution)-np.array(x_new))) < self.tolerance:
+                break
+
+            self.solution = [round(x, 5) for x in x_new]
+            iteration += 1
+
+        print(table)
+
+
 # A = [[2, -1, 0],
 #      [-1, 3, -1],
 #      [0, -1, 2]]
